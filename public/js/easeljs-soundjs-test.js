69177b01a04523c689f2ffad79e614fcdb9830a8
Poc275 <poc275@gmail.com>
2016-05-07 21:54:46 +0100

    Track movement snapped

--- public/js/easeljs-soundjs-test.js
+++ public/js/easeljs-soundjs-test.js
@@ -47,7 +47,6 @@ function init() {
 	playHead = new createjs.Shape();
 	playHead.graphics.beginStroke("orange");
 	playHead.graphics.moveTo(0, 0).lineTo(0, tracks.length * 50);
-	stage.addChild(playHead);
 
 	createjs.Sound.addEventListener('fileload', soundsLoaded);
 
@@ -61,8 +60,10 @@ function init() {
 	playButton.disabled = true;
 	stopButton.disabled = true;
 
+
 	for(var i = 0; i < tracks.length; i++) {
 		var track = new createjs.Shape();
+		track.name = tracks[i].title;
 
 		// register sounds
 		createjs.Sound.registerSound(tracks[i].url, tracks[i].title);
@@ -72,6 +73,7 @@ function init() {
 		track.graphics.beginFill(tracks[i].fill).drawRect(tracks[i].startTime, tracks[i].yPosition, tracks[i].length, tracks[i].height);
 		track.setBounds(tracks[i].startTime, tracks[i].yPosition, tracks[i].length, tracks[i].height);
 
+		// MOUSE EVENTS
 		track.on("mousedown", function(e) {
 			// retrieve position of mouse selection and set as transform point
 			// (regX) so that the tracks move smoothly
@@ -85,14 +87,51 @@ function init() {
 			stage.update();
 		});
 
+		track.on("pressup", function(e) {
+			// use regX as the local x position because it is applied only once on mousedown
+			// e.localX can change if the user keeps dragging even when the track is at its limits
+			var localXClamped = e.currentTarget.regX;
+			var lhs = e.currentTarget.x - localXClamped;
+
+			if (localXClamped < 0) {
+				localXClamped = 0;
+				lhs = 0;
+			} else if (localXClamped > e.currentTarget.getBounds().width) {
+				localXClamped = e.currentTarget.getBounds().width;
+				lhs = e.currentTarget.getBounds().width;
+			}
+
+			var snapPos = 50 * Math.round(lhs / 50);
+
+			e.currentTarget.x = snapPos + e.currentTarget.regX;
+
+			// console.log("xPos: " + e.currentTarget.x + " localX: " + e.localX + " localXClamped: " + localXClamped + " lhs: " + lhs + " snapPos: " + snapPos);
+
+			stage.update();
+
+			// which track was moved?
+			var track = tracks.find(function(track) {
+				if (track.title === e.currentTarget.name) {
+					return track;
+				}
+			});
+
+			// update track start time
+			track.instance.startTime = 20000;
+
+		});
+
 		stage.addChild(track);
 	}
+
+	// add play head at the end on top
+	stage.addChild(playHead);
 
 	stage.update();
 }
 
 // note: this will run for every sound that is loaded
-// need a way of waiting until ALL sounds are loaded
+// need a way of waiting until ALL sounds are loaded - preload.js?
 function soundsLoaded(evt) {
 	playButton.disabled = false;
 	stopButton.disabled = false;
@@ -143,7 +182,8 @@ function stopButtonClick(evt) {
 
 
 function syncPlayhead() {
-	console.log(tracks[0].instance.position / 1000 * playHeadNormalisationFactor);
+	// console.log(tracks[0].instance.position / 1000 * playHeadNormalisationFactor);
+	console.log(tracks[0].instance.position);
 	playHead.x = tracks[0].instance.position / 1000 * playHeadNormalisationFactor;
 	stage.update();
 	animationFrameId = requestAnimationFrame(syncPlayhead);
